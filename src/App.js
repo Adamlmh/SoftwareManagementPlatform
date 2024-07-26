576421c92b47408ad3fa3983e2852d2fe6fc31e0
L-L777 <148612270+L-L777@users.noreply.github.com>
2024-07-26 22:47:02 +0800

    feat layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import Router from "./router" 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Router />
     </div>
   );
 }
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,25 @@
 import { lazy } from "react";
 import { Suspense } from "react";
-const Login = lazy(() => import("../pages/login"));
+// const Login = lazy(() => import("../pages/login"));
+const TopMenu = lazy(() => import("../pages/layout"));
 
 const routes = [
+  // {
+  //   path: "/login",
+  //   element: (
+  //     <Suspense fallback={<div>Loading...</div>}>
+  //       <Login />
+  //     </Suspense>
+  //   ),
+  // },
   {
-    path: "/login",
+    path:"/",
     element: (
       <Suspense fallback={<div>Loading...</div>}>
-        <Login />
+        <TopMenu />
       </Suspense>
     ),
-  },
+  }
 ];
 
 export default routes;
