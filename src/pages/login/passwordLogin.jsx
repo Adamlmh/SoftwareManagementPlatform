59eab40d3cc2cec60ecc7c86e880e4d608968c84
wuminglaogou <2132772270@qq.com>
2024-07-27 17:20:51 +0800

    000

--- src/pages/login/passwordLogin.jsx
+++ src/pages/login/passwordLogin.jsx
@@ -1,41 +1,95 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Card, Input } from 'antd'
 
-export default function passwordLogin() {
-    return (
-        <div className="login_center">
-            <div className="login_center_card">
-                <Card style={{ width: '520px', height: '570px', padding: '0px', margin: '0px' }} className="custom-card">
-                    <div className="select_login_register">
-                        <h3 className="select_login_register_left select_login_register_active">登录</h3>
-                        <h3 className="select_login_register_right">注册</h3>
-                    </div>
-                    <div className="login_center_card_bottom">
-                        <h3>欢迎使用工软软件管理平台</h3>
-                        <div><Input placeholder="输入你的邮箱" /></div>
-                        <div><Input placeholder="输入你的密码" type="password" /></div>
-                        <div className="remberMeBigDiv"><div><input type="checkbox" />记住我</div></div>
-                    </div>
-                    <div className="login_center_card_bottom_bottom">
-                        <div><button>登录</button></div>
-                        <div><div><input type="checkbox" />已同意<a >《服务条款》</a>与<a >《隐私政策》</a></div>  </div>
-                    </div>
 
-                    {/* <div className="select_login_register">
-                        <h3 className="select_login_register_left">登录</h3>
-                        <h3 className="select_login_register_right select_login_register_active">注册</h3>
-                    </div>
-                    <div className="register_center_card_bottom">
-                        <div><Input placeholder="输入你的邮箱" /></div>
-                        <div><Input placeholder="输入你的密码" type="password" /></div>
-                        <div><Input placeholder="确认密码" type="password" /></div>
-                        <div><Input placeholder="验证码" type="password" /><button>验证码图片</button></div>
-                    </div>
-                    <div className="login_center_card_bottom_bottom">
-                        <div><button>注册</button></div>
-                        <div><div><input type="checkbox" />已同意<a href="#">《服务条款》</a>与<a href="#">《隐私政策》</a></div>  </div>
-                    </div> */}
-                </Card>
+export default function PasswordLogin({ alert, setAlert }) {
+    const passwordRef = useRef()
+    const emailRef = useRef()
+    const rembermeRef = useRef()
+    const agreeRef = useRef()
+
+    //定时器提示
+    const setAlertTimeout = (setter, alert, duration = 3000, type = 0) => {
+        if (type == 0) {
+            setter(alert)
+            setTimeout(() => {
+                setter({ message: '', type: "" })
+            }, duration)
+        }
+        else {
+            setter(alert)
+            setTimeout(() => {
+                setter({ message: '', type: "" })
+                // navigate('/home')
+            }, duration)
+
+        }
+    }
+
+    //登录按钮
+    const login_In = async () => {
+        const password = passwordRef.current.input.value
+        const email = emailRef.current.input.value
+        const agree = agreeRef.current.checked
+        if (!email) {
+            setAlertTimeout(setAlert, { message: '请输入邮箱', type: 'error' });
+            return;
+        }
+        if (!password) {
+            setAlertTimeout(setAlert, { message: '请输入密码', type: 'error' });
+            return;
+        }
+        if (!agree) {
+            setAlertTimeout(setAlert, { message: '请同意相关条款政策', type: 'error' });
+            return;
+        }
+        // try {
+        //     const response = await accountLogin(email, password);
+        //     console.log(response)
+        //     if (response.code == 1) {
+        //         setAlertTimeout(setAlert, { message: '登录成功', type: 'success' }, 1000, 1);
+        //         localStorage.setItem('token', response.data.token)
+        //         localStorage.setItem('userId', response.data.userId)
+        //         localStorage.setItem('email', email)
+        //         if (rembermeRef.current.checked) {
+        //             localStorage.setItem('password', password)
+        //             localStorage.setItem('email', email)
+        //         }
+        //     }
+        //     else {
+        //         setAlertTimeout(setAlert, { message: response.msg, type: 'error' });
+        //     }
+        // } catch (error) {
+        //     console.error('Error fetching models:', error);
+        // }
+    }
+
+
+
+    useEffect(() => {
+
+        if (localStorage.getItem('passwordSF')) {
+            passwordRef.current.input.value = localStorage.getItem('password')
+        }
+        if (localStorage.getItem('emaiSF')) {
+            emailRef.current.input.value = localStorage.getItem('email')
+        }
+        rembermeRef.current.checked = true
+        agreeRef.current.checked = true
+    }, [])
+
+
+    return (
+        <div>
+            <div className="login_center_card_bottom">
+                <h3>欢迎使用工软软件管理平台</h3>
+                <div><Input placeholder="输入你的邮箱" ref={emailRef} /></div>
+                <div><Input placeholder="输入你的密码" ref={passwordRef} type="password" /></div>
+                <div className="remberMeBigDiv"><div><input type="checkbox" ref={rembermeRef} />记住我</div></div>
+            </div>
+            <div className="login_center_card_bottom_bottom">
+                <div><button onClick={login_In} >登录</button></div>
+                <div><div><input type="checkbox" ref={agreeRef} />已同意<a>《服务条款》</a>与<a >《隐私政策》</a></div>  </div>
             </div>
         </div>
     )
