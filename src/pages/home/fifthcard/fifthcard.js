599683880ee2799e957739b50959bd185e41a3bd
L-L777 <148612270+L-L777@users.noreply.github.com>
2024-07-28 15:14:03 +0800

    feat home's fifth page

--- src/pages/home/fifthcard/fifthcard.js
+++ src/pages/home/fifthcard/fifthcard.js
@@ -1,10 +1,16 @@
 import styles from "./card.module.css"
+
 import { Card } from 'antd';
-const FifthCard = () => {
+const FifthCard = ({image}) => {
     return (
         <Card
             className={styles.card}
-            hoverable bordered={false} style={{ width: '30%'}}>
+            hoverable  style={{ width: '23%' }}>
+           
+                <div className={styles.picbox}>
+                <img className={styles.pic} src={image}></img>
+                </div>
+                
         </Card>
     )
 }
