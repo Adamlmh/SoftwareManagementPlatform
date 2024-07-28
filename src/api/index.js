7ff0ee1493e18611ccea96969310690b97bda5a2
wuminglaogou <2132772270@qq.com>
2024-07-28 23:43:44 +0800

    0

--- src/api/index.js
+++ src/api/index.js
@@ -12,12 +12,13 @@ export function loginAPI(data) {
 //发送验证码
 export function sendVerificationCode(email) {
   console.log(`发送验证码`)
-  // const data = {
-  //   email: email,
-  // };
-  // return service({
-  //   url: "/account/register/sendVerificationCode",
-  //   method: "post",
-  //   data,
-  // });
+  const data = {
+    email: email,
+  };
+  console.log(data)
+  return service({
+    url: "http://47.113.224.195:31108/account/sendVerificationCode",
+    method: "post",
+    data,
+  });
 }
