074461085d73d2712cb8cc59187d963dc94aeee2
wuminglaogou <2132772270@qq.com>
2024-07-30 16:59:53 +0800

    000

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,10 +1,28 @@
 import "./layout.css";
-
+import { useEffect } from 'react'
 import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
 import TopHeader from "./topheader";
 import BottomFooter from "./bottomfooter";
+import { homePageUserInfo } from '../../api/index'
 const TopMenu = () => {
+
+  useEffect(() => {
+    async function receiveInformation() {
+      try {
+        const response = await homePageUserInfo(localStorage.getItem('userIdSf'))
+        console.log(response.data)
+        localStorage.setItem('userName', response.data.username)
+        localStorage.setItem('userImage', response.data.image)
+      } catch (error) {
+        console.error('Error sending verification code:', error);
+      }
+
+    }
+    receiveInformation()
+  }, [])
+
+
   return (
     <Layout>
       <TopHeader></TopHeader>
